e4dae535d9a6e0b62df46e4c6d504297b8703a92
MatheusGatto78 <matheusviniciusgatto@gmail.com>
2026-04-17 11:29:24 -0300

    Qual o total de custo por Store_type e qual o % do total de custos?

--- main.py
+++ main.py
@@ -20,24 +20,43 @@ except Exception as e:
 # total_valor_vendas = df['Total_Cost'].sum()
 # print(f"Total de valor de vendas: R$ {total_valor_vendas:.2f}")
 
-# Quantos itens cada Store_type vendeu e quantos % representa do total de itens vendidos?
-# Itens vendidos por Store_type
-total_itens_vendidos = df['Total_Items'].sum()
-itens_por_loja = df.groupby('Store_Type')['Total_Items'].sum().sort_values(ascending=False)
-print("\nItens vendidos por tipo de loja:")
-print(itens_por_loja)
+# # Quantos itens cada Store_type vendeu e quantos % representa do total de itens vendidos?
+# # Itens vendidos por Store_type
+# total_itens_vendidos = df['Total_Items'].sum()
+# itens_por_loja = df.groupby('Store_Type')['Total_Items'].sum().sort_values(ascending=False)
+# print("\nItens vendidos por tipo de loja:")
+# print(itens_por_loja)
+# # Percentual de itens vendidos por Store_type
+# percentual_itens_por_loja = (itens_por_loja / total_itens_vendidos) * 100
+# print("\nPercentual de itens vendidos por tipo de loja:")
+# print(percentual_itens_por_loja.apply(lambda x: f'{x:.2f}%'))
+# # Visualização
+# plt.figure(figsize=(12, 7))
+# sns.barplot(x=itens_por_loja.index, y=itens_por_loja.values, hue=itens_por_loja.index, palette='viridis', legend=False)
+# plt.title('Total de Itens Vendidos por Tipo de Loja')
+# plt.xlabel('Tipo de Loja')
+# plt.ylabel('Total de Itens Vendidos')
+# plt.xticks(rotation=45)
+# plt.tight_layout()
+# plt.show()
 
-# Percentual de itens vendidos por Store_type
-percentual_itens_por_loja = (itens_por_loja / total_itens_vendidos) * 100
-print("\nPercentual de itens vendidos por tipo de loja:")
-print(percentual_itens_por_loja.apply(lambda x: f'{x:.2f}%'))
+# Qual o total de custo por Store_type e qual o % do total de custos?
+total_custo_geral = df['Total_Cost'].sum()
+
+custo_por_loja = df.groupby('Store_Type')['Total_Cost'].sum().sort_values(ascending=False)
+print("\nTotal de custo por tipo de loja:")
+print(custo_por_loja)
+
+percentual_custo_por_loja = (custo_por_loja / total_custo_geral) * 100
+print("\nPercentual de custo por tipo de loja:")
+print(percentual_custo_por_loja.apply(lambda x: f'{x:.2f}%'))
 
 # Visualização
 plt.figure(figsize=(12, 7))
-sns.barplot(x=itens_por_loja.index, y=itens_por_loja.values, hue=itens_por_loja.index, palette='viridis', legend=False)
-plt.title('Total de Itens Vendidos por Tipo de Loja')
+sns.barplot(x=custo_por_loja.index, y=custo_por_loja.values, hue=custo_por_loja.index, palette='magma', legend=False)
+plt.title('Total de Custo por Tipo de Loja')
 plt.xlabel('Tipo de Loja')
-plt.ylabel('Total de Itens Vendidos')
+plt.ylabel('Total de Custo')
 plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
