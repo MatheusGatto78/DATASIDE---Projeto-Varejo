578b8146bca4b111995011e5ae5912ed13601063
MatheusGatto78 <matheusviniciusgatto@gmail.com>
2026-04-17 09:52:23 -0300

    Qual o valor total de vendas?

--- main.py
+++ main.py
@@ -7,28 +7,15 @@ plt.rcParams['figure.figsize'] = (10, 6)
 
 try:
     df = pd.read_csv('Retail_Transactions_Dataset.csv')
-    # print("Dataset carregado com sucesso!")
-    # print("\nPrimeiras 5 linhas do dataset:")
-    # print(df.head())
-    # print("\nInformações gerais do dataset:")
-    # df.info()
-    # print("\nEstatísticas descritivas do dataset:")
-    # print(df.describe())
 except FileNotFoundError:
     print("Erro: O arquivo 'Retail_Transactions_Dataset.csv' não foi encontrado. Certifique-se de que o arquivo está no diretório correto.")
 except Exception as e:
     print(f"Ocorreu um erro ao carregar o dataset: {e}")
 
-# print("\nValores ausentes por coluna:")
-# print(df.isnull().sum())
+# #  Qual o total de itens vendidos (somar a quantidade de todos os itens)?
+# total_itens_vendidos = df['Total_Items'].sum()
+# print(f"\nTotal de itens vendidos: {total_itens_vendidos}")
 
-# print("\nTipos de dados por coluna:")
-# print(df.dtypes)
-
-# print(f"\nNúmero de linhas duplicadas antes da remoção: {df.duplicated().sum()}")
-# df.drop_duplicates(inplace=True)
-# print(f"Número de linhas após remoção de duplicatas: {len(df)}")
-
-#  Qual o total de itens vendidos (somar a quantidade de todos os itens)?
-total_itens_vendidos = df['Total_Items'].sum()
-print(f"\nTotal de itens vendidos: {total_itens_vendidos}")
+# Qual o valor total de vendas?
+total_valor_vendas = df['Total_Cost'].sum()
+print(f"Total de valor de vendas: R$ {total_valor_vendas:.2f}")
